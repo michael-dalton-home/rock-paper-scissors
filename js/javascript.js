e763ca7e193572d80c6aea012006646878771539
Michael Dalton <215679523+michael-dalton-home@users.noreply.github.com>
2025-07-21 12:30:58 +0100

    Display running score and announce the winner
    Allows for 5 rounds before announcing a winner after which
    a further press starts a new game.

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,30 @@
 
+// Scores
 let humanScore = 0;
 let computerScore = 0;
+
+// Hooks to update game status in UI
+const gameStatus = document.querySelector('#game-status');
+const gameScore  = document.querySelector('#game-score');
+
+// Constants
+const ROCK      = 'rock';
+const PAPER     = 'paper';
+const SCISSORS  = 'scissors';
+const WINNING_SCORE = 5
+
+
 
 // write a function that randomly returns “rock”, “paper” or “scissors”.
 function getComputerChoice() {
     rnd = Math.random();
 
     if (rnd <= 0.333)
-        return "rock";
+        return ROCK;
     else if (rnd <= 0.666)
-        return "paper";
+        return PAPER;
     else
-        return "scissors";
+        return SCISSORS;
 }
 
 // write a function that takes the user choice and returns it.
@@ -24,25 +37,40 @@ function getHumanChoice() {
 /* In: humanChoice, computerChoice - both player choices
 */
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+    // humanChoice = humanChoice.toLowerCase();
+
 
     if (humanChoice === computerChoice) {
         console.log(`TIE: Both ${humanChoice}`);
+        gameStatus.textContent = `It's a draw - you both chose ${humanChoice}`;
     }
-    else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper') ||
-        (humanChoice === 'paper' && computerChoice === 'rock')) {
-            console.log(`WIN: You:${humanChoice}, Cpu:${computerChoice}`);
+    else if ((humanChoice === ROCK && computerChoice === SCISSORS) ||
+        (humanChoice === SCISSORS && computerChoice === PAPER) ||
+        (humanChoice === PAPER && computerChoice === ROCK)) {
+            gameStatus.textContent = `You WON that round - ${humanChoice}`+
+                ` beats ${computerChoice}`;
             humanScore ++;
+            gameScore.textContent = `You: ${humanScore} Computer: ${computerScore}`;
         }
-    else if ((computerChoice === 'rock' && humanChoice === 'scissors') ||
-        (computerChoice === 'scissors' && humanChoice === 'paper') ||
-        (computerChoice === 'paper' && humanChoice === 'rock')) {
-            console.log(`LOSE: You:${humanChoice}, Cpu:${computerChoice}`);
+    else if ((computerChoice === ROCK && humanChoice === SCISSORS) ||
+        (computerChoice === SCISSORS && humanChoice === PAPER) ||
+        (computerChoice === PAPER && humanChoice === ROCK)) {
+            gameStatus.textContent = `You LOST that round - ${humanChoice}`+
+                ` loses to ${computerChoice}`;
             computerScore ++;
+            gameScore.textContent = `You: ${humanScore} Computer: ${computerScore}`;
         }
     else
         winner = `missing h${humanChoice}:c${computerChoice}`;
+
+    if (computerScore >= WINNING_SCORE) {
+        gameStatus.textContent += `\nCOMPUTER wins the GAME`;
+        gameReset(false);
+    }
+    else if (humanScore >= WINNING_SCORE) {
+        gameStatus.textContent += `\nYOU win the GAME.  Well done`;
+        gameReset(false);
+    }
 }
 
 function playGame() {
@@ -62,9 +90,9 @@ function manageButtonClicks(e) {
         let humanChoice = 'none';
 
         switch (e.target.id) {
-        case 'r-button': humanChoice = 'rock';      break;
-        case 'p-button': humanChoice = 'paper';     break;
-        case 's-button': humanChoice = 'scissors';  break;
+        case 'r-button': humanChoice = ROCK;      break;
+        case 'p-button': humanChoice = PAPER;     break;
+        case 's-button': humanChoice = SCISSORS;  break;
         }
 
         if (humanChoice !== 'none') {
@@ -74,16 +102,23 @@ function manageButtonClicks(e) {
     }
 }
 
+function gameReset(ui) {
+    humanScore = 0;
+    computerScore = 0;
+
+    if (ui) {
+        gameStatus.textContent = `Game ready.  Make a choice using the buttons above.`;
+        gameScore.textContent = `You: ${humanScore} Computer: ${computerScore}`;
+    }
+}
+
 /* MAIN
 */
 {
-    /*for (let i=0; i<10; i++)
-            console.log(`${getComputerChoice()}`);
+    // Setup initial state
+    gameReset(true);
 
-    console.log(` ${getHumanChoice()});*/
-
-    // playGame();
-
+    // Setup callbacks for human choice buttons
     const myBody = document.querySelector('.body-content');
 
     myBody.addEventListener('click', manageButtonClicks);
