f7c8583ee173f1399cee65ffb381af8098d947c1
Michael Dalton <215679523+michael-dalton-home@users.noreply.github.com>
2025-07-21 10:05:49 +0100

    Add three buttons to UI representing rock, paper, scissors
    Human can now choose options using mouse clicks on buttons.  Added
    css styling and updated html appropriately.  Javascript then makes use
    of new html objects.  No relfection of scores yet though

--- js/javascript.js
+++ js/javascript.js
@@ -48,9 +48,46 @@ function playRound(humanChoice, computerChoice) {
 function playGame() {
     console.log("Let's play ...(5 rounds)");
 
-    for (let i=0; i<5; i++) {
+    // for (let i=0; i<5; i++) {
         playRound(getHumanChoice(), getComputerChoice());
         console.log(`Score: ${humanScore}:${computerScore}`);
+    //}
+
+}
+
+function manageButtonClicks(e) {
+    console.log(`Button clicked ${e.target.id}`);
+
+    if (e.type === 'click') {
+        let humanChoice = 'none';
+
+        switch (e.target.id) {
+        case 'r-button': humanChoice = 'rock';      break;
+        case 'p-button': humanChoice = 'paper';     break;
+        case 's-button': humanChoice = 'scissors';  break;
+        }
+
+        if (humanChoice !== 'none') {
+            playRound(humanChoice, getComputerChoice());
+            console.log(`Score: ${humanScore}:${computerScore}`);
+        }
     }
+}
+
+/* MAIN
+*/
+{
+    /*for (let i=0; i<10; i++)
+            console.log(`${getComputerChoice()}`);
+
+    console.log(` ${getHumanChoice()});*/
+
+    // playGame();
+
+    const myBody = document.querySelector('.body-content');
+
+    myBody.addEventListener('click', manageButtonClicks);
+
+    console.log("End.");
 
 }
